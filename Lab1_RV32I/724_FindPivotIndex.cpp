0bad6f0b656af4c7481d2b712663df3f1012c341
abnormal749 <40458788+abnormal749@users.noreply.github.com>
2022-10-10 14:52:56 +0800

    update

---
+++ Lab1_RV32I/724_FindPivotIndex.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+    }
+};
+
+int main(int arg, char* argv[]){
+   int test[] = {1,7,3,6,5,6};
+
+   return 0;
+}
