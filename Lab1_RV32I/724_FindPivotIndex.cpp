f1c6dcf6cb1da6a6c3b154b24a308830f8ed8aa3
abnormal749 <40458788+abnormal749@users.noreply.github.com>
2022-10-10 15:32:28 +0800

    update

--- Lab1_RV32I/724_FindPivotIndex.cpp
+++ Lab1_RV32I/724_FindPivotIndex.cpp
@@ -1,15 +1,34 @@
-#include<stdio.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+	int leftSum=0,rightSum=0;
+        for(int i=0; i<nums.size(); i++)
+	    rightSum += nums[i];
+
+	for(int i=0; i<nums.size(); i++){
+	    rightSum -= nums[i];
+	    if(leftSum == rightSum){
+	        return i;
+	    }else{
+	        leftSum += nums[i];
+	    }
+	}
+
+	return -1;
     }
 };
 
 int main(int arg, char* argv[]){
-   int test[] = {1,7,3,6,5,6};
+    vector<int> test1 = {1,7,3,6,5,6};
+    //vector<int> test2 = {1,2,3};
+    vector<int> test2 = {2,1,-1};
 
-   return 0;
+    Solution a;
+    std::cout << a.pivotIndex(test2) << '\n';
+    return 0;
 }
