80d84a8e50deff9f4205fb15e5b87c3be27f3cac
abnormal749 <40458788+abnormal749@users.noreply.github.com>
2022-10-10 16:31:48 +0800

    Update

--- Lab1_RV32I/724_FindPivotIndex.s
+++ Lab1_RV32I/724_FindPivotIndex.s
@@ -7,15 +7,39 @@ str:   .string "The answer is "
 
 main:
     lw   s1, len
-    add 
+    la   s2, input
+    add  t0, zero,zero     #i=0
+    add  t1, zero,zero     #Left sum
+    add  t2, zero,s3       #Right sum
+    add  s3, zero,s2       
+sumLoop:
+    lw   t3, 0(s3)         #nums[i]
+    add  t2, t2,t3         #rightSum += nums[i]
+    addi s3, s3, 4         #Pointer sheft
+    addi t0, t0, 1         #i++
+    blt  t0, s1, sumLoop
 
+    add  t0, zero,zero     #i=0
+    add  s3, zero,s2
+findLoop:
+    lw   t3, 0(s3)         #nums[i]
+    sub  t2, t2, t3        #rightSum -= nums[i]
+    beq  t1, t2, foundPI   #leftSum == rightSum
+    add  t1, t2, t3        #leftSum += nums[i]
+    addi t0, t0, 1         #i++
+    blt  t0, s1, findLoop
 
-finishP:
-    la a0, str             #string
-    li a7, 4               #print string
+    addi s4, zero, -1      #Not found
+printAns:
+    la a0, str             #String
+    li a7, 4               #Print string
     ecall
-    add a0, s2, zero       #load answer
-    li a7, 1               #print max
+    add a0, s4, zero       #Load answer
+    li a7, 1               #Print answer
     ecall
-    li a7, 10              #end program
+    li a7, 10              #End program
     ecall
+
+foundPI:
+    add  s4, t0, zero
+    j printAns
